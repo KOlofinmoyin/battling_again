1fe70eeb5563ff0a6512a444ad4a00a940baf5fe
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-03-08 17:57:03 +0000

    - required the Player class in the controller file, app.rb - stored player names as attributes of instances of the Player class in global variables (declared using $) in-lieu strings in the session. - Updated the instance variables to reference the Player instances rather than the session keys

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'player'
 
 class Battling_again < Sinatra::Base
   enable :sessions
@@ -8,20 +9,20 @@ class Battling_again < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1] = params[:player_1]
-    session[:player_2] = params[:player_2]
+    $player_1 = Player.new(params[:player_1])
+    $player_2 = Player.new(params[:player_2])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = session[:player_1]
-    @player_2 = session[:player_2]
+    @player_1 = $player_1
+    @player_2 = $player_2
     erb :play
   end
 
   get '/attack' do
-    @player_1 = session[:player_1]
-    @player_2 = session[:player_2]
+    @player_1 = $player_1
+    @player_2 = $player_2
     erb :attack
   end
 
