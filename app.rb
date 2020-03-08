f8b188f6fdc5b943f459d875afe86ae7b1c569a9
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-03-08 18:18:31 +0000

    As Player 1 (delivered), So I can start to win a game of Battle, I want my attack to reduce Player 2's HP by 10

--- app.rb
+++ app.rb
@@ -23,6 +23,7 @@ class Battling_again < Sinatra::Base
   get '/attack' do
     @player_1 = $player_1
     @player_2 = $player_2
+    @player_1.attack(@player_2)
     erb :attack
   end
 
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,4 +3,11 @@ feature 'Player hit Points' do
     sign_in_and_play
     expect(page).to have_content 'Mittens: 60HP'
   end
+
+  scenario 'reduce Player 2\'s HP' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).not_to have_content('Mittens: 60HP')
+    expect(page).to have_content('Mittens: 50HP')
+  end
 end
