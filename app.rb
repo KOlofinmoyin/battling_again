c6a902cf04e473e54ebf8406eb29cf917f7a86cc
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-03-10 23:26:59 +0000

    Refactored the controller, to make it 'skinny'.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
-require 'player'
 require 'game'
+require 'player'
+
 
 class Battling_again < Sinatra::Base
   enable :sessions
@@ -10,21 +11,20 @@ class Battling_again < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1])
-    $player_2 = Player.new(params[:player_2])
+    player_1 = Player.new(params[:player_1])
+    player_2 = Player.new(params[:player_2])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $player_1
-    @player_2 = $player_2
+    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    Game.new.attack(@player_2)
+    @game = $game
+    @game.attack(@game.player_2)
     erb :attack
   end
 
