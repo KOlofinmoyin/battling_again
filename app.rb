43be25f1ca27cf20271895aa702e41cb9bed2b18
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-03-11 20:28:50 +0000

    nailed down the validation of turns between players.

--- app.rb
+++ app.rb
@@ -25,6 +25,7 @@ class Battling_again < Sinatra::Base
   get '/attack' do
     @game = $game
     @game.attack(@game.player_2)
+    @game.switch_turn
     erb :attack
   end
 
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -13,9 +13,19 @@ feature 'Attack' do
     expect(page).to have_content('Mittens\'s turn')
   end
 
-  # scenario 'Player 1 wants attack' do
-  #   sign_in_and_play
-  #   expect(page).to have_content('Dave\'s turn')
-  #   expect(page).not_to have_content('Mittens\'s turn')
-  # end
+  scenario 'Player 1 wants attack' do
+    sign_in_and_play
+    expect(page).not_to have_content('Mittens\'s turn')
+    expect(page).to have_content('Dave\'s turn')
+  end
+
+  scenario 'Player 1 attacks Player2, then Player2 hits Player1 and turn reverts to Player1' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_on 'Ok'
+    click_button 'Attack'
+    click_on 'Ok'
+    expect(page).not_to have_content('Mittens\'s turn')
+    expect(page).to have_content('Dave\'s turn')
+  end
 end
