6414f3ee2e50dbd41481085c3f59f4b2adf2acb1
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-03-12 11:25:04 +0000

    created an #attack_and_confirm spec helper!

--- spec/features/lose_game_spec.rb
+++ spec/features/lose_game_spec.rb
@@ -1,27 +1,8 @@
 feature 'Lose' do
   scenario 'player loses a game if they reach 0HP first and a message is displayed.' do
+    exchange_attacks = 11
     sign_in_and_play
-    click_on 'Attack'
-    click_on 'Ok'
-    click_on 'Attack'
-    click_on 'Ok'
-    click_on 'Attack'
-    click_on 'Ok'
-    click_on 'Attack'
-    click_on 'Ok'
-    click_on 'Attack'
-    click_on 'Ok'
-    click_on 'Attack'
-    click_on 'Ok'
-    click_on 'Attack'
-    click_on 'Ok'
-    click_on 'Attack'
-    click_on 'Ok'
-    click_on 'Attack'
-    click_on 'Ok'
-    click_on 'Attack'
-    click_on 'Ok'
-    click_on 'Attack'
+    exchange_attacks.times { attack_and_confirm }
     save_and_open_page
     expect(page).to have_content 'Mittens loses!'
   end
